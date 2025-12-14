6646a3e465c9cef262ab9a5994b5567fb3a0800f
theofficialomega <30985701+BellezaEmporium@users.noreply.github.com>
2025-12-14 12:35:24 +0100

    marko v5/v6 adapt

--- taglib/i18n-message.js
+++ taglib/i18n-message.js
@@ -1,5 +1,25 @@
-'use strict';
+"use strict";
 
-module.exports = function (input, out) {
-  out.write(out.stream.req.t(input.key, input['*'] || {}));
+/**
+ * Marko v6 renderer
+ * Contract: out.global.t(key, options)
+ */
+module.exports = function i18nMessage(input, out) {
+  const t = out.global && out.global.t;
+
+  // Options bag comes from @* attributes
+  const options = input["*"] || {};
+
+  const value = t
+    ? t(input.key, options)
+    : input.key;
+
+  const shouldEscape =
+    input.escape === undefined ? true : Boolean(input.escape);
+
+  if (shouldEscape) {
+    out.write(out.escapeXml(String(value)));
+  } else {
+    out.write(String(value));
+  }
 };
--- taglib/i18n-var-tag.js
+++ taglib/i18n-var-tag.js
@@ -1,8 +1,60 @@
-'use strict';
+import { types as t } from "@marko/compiler";
+"use strict";
 
-module.exports = function (el, codegen) {
-  const builder = codegen.builder;
-  const nameAttrValue = el.getAttributeValue('name');
-  const name = nameAttrValue ? nameAttrValue.value : null;
-  return builder.var(name || 'i18n', 'out.stream.req.t');
-};
+/**
+ * <i18n-var name="i18n" />
+ *
+ * Compiles to:
+ * const i18n = (key, opts) =>
+ *   out.global.t ? out.global.t(key, opts || {}) : key;
+ */
+export default function i18nVarTransform(tag) {
+  const nameAttr = tag.node.attributes.find(
+    (attr) =>
+      t.isMarkoAttribute(attr) &&
+      attr.name === "name" &&
+      t.isStringLiteral(attr.value)
+  );
+
+  const identifier = nameAttr
+    ? nameAttr.value.value
+    : "i18n";
+
+  tag.replaceWith(
+    t.variableDeclaration("const", [
+      t.variableDeclarator(
+        t.identifier(identifier),
+        t.arrowFunctionExpression(
+          [t.identifier("key"), t.identifier("opts")],
+          t.conditionalExpression(
+            t.memberExpression(
+              t.memberExpression(
+                t.identifier("out"),
+                t.identifier("global")
+              ),
+              t.identifier("t")
+            ),
+            t.callExpression(
+              t.memberExpression(
+                t.memberExpression(
+                  t.identifier("out"),
+                  t.identifier("global")
+                ),
+                t.identifier("t")
+              ),
+              [
+                t.identifier("key"),
+                t.logicalExpression(
+                  "||",
+                  t.identifier("opts"),
+                  t.objectExpression([])
+                )
+              ]
+            ),
+            t.identifier("key")
+          )
+        )
+      )
+    ])
+  );
+}
